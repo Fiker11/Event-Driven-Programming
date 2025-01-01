c88631d5522b7511739c22f9f0dfe56a57719a97
Fiker11 <149205732+Fiker11@users.noreply.github.com>
2025-01-01 19:25:06 +0300

    depedency injection

--- StopWatch/obj/Debug/net8.0/StopWatch.AssemblyInfo.cs
+++ StopWatch/obj/Debug/net8.0/StopWatch.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StopWatch")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09bbc66ac33d0826221860a5115412497f2ba6e5")]
 [assembly: System.Reflection.AssemblyProductAttribute("StopWatch")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StopWatch")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
